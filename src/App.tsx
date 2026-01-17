e724520540612db7dc67cc4bf92f8ceda3f1dd84
souma9830 <prabirkumarshee8@gmail.com>
2026-01-17 21:25:04 +0530

    Modified

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Layout } from './components/Layout';
 import { Home } from './pages/Home';
 import { BlogDetail } from './pages/BlogDetail';
 import { CreateBlog } from './pages/CreateBlog';
+import { Bookmarks } from './pages/Bookmarks';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/blogs/:id" element={<BlogDetail />} />
         <Route path="/create" element={<CreateBlog />} />
+        <Route path="/bookmarks" element={<Bookmarks />} />
       </Routes>
     </Layout>
   );
